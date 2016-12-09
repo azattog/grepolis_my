02b9760e73ac33b0c23d2ad56857640a36509ae6
azattog <6069707@gmail.com>
2016-12-09 23:47:08 +0800

    первая заливка

---
+++ ver2_grep.user.js
@@ -0,0 +1,13 @@
+
+
+// ==UserScript==
+// @name скрипт для нашего альянса
+// @namespace грепа
+// @version 0.01
+// @source пока пусто
+// @description по поводу работы этого скрипта спрашивайте всю инфу у босов
+// @include *grepolis.com*
+// ==/UserScript==
+
+
+alert('все ахуенно, установились');
